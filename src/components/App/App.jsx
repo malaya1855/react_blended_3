2ec3de69ed1565de29767f93ef381e79b9b5e0d0
malaya1855 <121491813+malaya1855@users.noreply.github.com>
2023-07-29 13:01:39 +0100

    start App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,16 @@
 import { Header } from 'components';
 import { CountrySearch, Home, Country } from 'pages';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const App = () => {
-  return <h2>App</h2>;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/country" element={<CountrySearch />} />
+        <Route path="/country/:countryId" element={<Country />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { GlobalStyles, theme } from 'styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename = '/react_blended_3'>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
         <App />
