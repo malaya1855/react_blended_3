64a3a90f8e490ad3cf6caeff2d3f1703fa9a322f
immortalua96 <romanshvagla@icloud.com>
2023-07-29 15:13:15 +0200

    page-search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,29 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
-export const SearchForm = () => {
+export const SearchForm = ({onSubmit}) => {
+  const [region , setRegion] = useState('')
+  const onChangeRegoin = (e)=>{
+    setRegion(e.target.value)
+  }
+const handleSubmit = (e)=>{
+  e.preventDefault()
+  if(!region){
+    alert ("виберіть регіон")
+    return
+  }
+  onSubmit(region)
+  setRegion('')
+  
+}
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select aria-label="select" name="region" required onChange={onChangeRegoin}defaultValue="default" >
+        <option value='default' disabled >
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,26 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect,useState } from 'react';
+import { useSearchParams, } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const  [searchParams , setSearchParams ] = useSearchParams()
+  const [countries, setCountries] = useState([])
+  useEffect(()=>{
+  const region = searchParams.get('region')
+  if(!region) return;
+  fetchByRegion(region).then(setCountries)
+  },[searchParams])
+  const onSubmit = (region)=>{
+   setSearchParams({region})
+  }
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+       <SearchForm onSubmit={onSubmit} />
+       <CountryList countries={countries} />
       </Container>
     </Section>
   );
